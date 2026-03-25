95712d0a6ebdfa1366927fee6f3b9896198783ad
jhosepr <rpj1010465@est.univalle.edu>
2026-03-25 18:15:49 -0400

    feat: index

---
+++ Controllers/MedicamentoesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using practica.Data;
+using practica.Models;
+
+namespace practica.Controllers
+{
+    public class MedicamentoesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MedicamentoesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Medicamentoes
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Medicamentos.Include(m => m.Categoria).Include(m => m.Estante);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Medicamentoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medicamento = await _context.Medicamentos
+                .Include(m => m.Categoria)
+                .Include(m => m.Estante)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (medicamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(medicamento);
+        }
+
+        // GET: Medicamentoes/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Nombre");
+            ViewData["EstanteId"] = new SelectList(_context.Estantes, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Medicamentoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Precio,Stock,FechaVencimiento,Estado,CategoriaId,EstanteId")] Medicamento medicamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(medicamento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Nombre", medicamento.CategoriaId);
+            ViewData["EstanteId"] = new SelectList(_context.Estantes, "Id", "Nombre", medicamento.EstanteId);
+            return View(medicamento);
+        }
+
+        // GET: Medicamentoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medicamento = await _context.Medicamentos.FindAsync(id);
+            if (medicamento == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Nombre", medicamento.CategoriaId);
+            ViewData["EstanteId"] = new SelectList(_context.Estantes, "Id", "Nombre", medicamento.EstanteId);
+            return View(medicamento);
+        }
+
+        // POST: Medicamentoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Precio,Stock,FechaVencimiento,Estado,CategoriaId,EstanteId")] Medicamento medicamento)
+        {
+            if (id != medicamento.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(medicamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MedicamentoExists(medicamento.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Nombre", medicamento.CategoriaId);
+            ViewData["EstanteId"] = new SelectList(_context.Estantes, "Id", "Nombre", medicamento.EstanteId);
+            return View(medicamento);
+        }
+
+        // GET: Medicamentoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medicamento = await _context.Medicamentos
+                .Include(m => m.Categoria)
+                .Include(m => m.Estante)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (medicamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(medicamento);
+        }
+
+        // POST: Medicamentoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var medicamento = await _context.Medicamentos.FindAsync(id);
+            if (medicamento != null)
+            {
+                _context.Medicamentos.Remove(medicamento);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MedicamentoExists(int id)
+        {
+            return _context.Medicamentos.Any(e => e.Id == id);
+        }
+    }
+}
